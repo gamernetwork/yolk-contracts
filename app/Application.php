9330255c28b7679dbb3da7d704accc0771ae3317
Simon Downes <simon.downes@gamer-network.net>
2015-06-30 11:54:40 +0100

    Application WiP

---
+++ app/Application.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * This file is part of Yolk - Gamer Network's PHP Framework.
+ *
+ * Copyright (c) 2015 Gamer Network Ltd.
+ * 
+ * Distributed under the MIT License, a copy of which is available in the
+ * LICENSE file that was bundled with this package, or online at:
+ * https://github.com/gamernetwork/yolk-contracts
+ */
+
+namespace yolk\contracts\app;
+
+interface Application {
+
+	/**
+	 * Runs the application.
+	 * @param $request
+	 * @return  the response to the request
+	 */
+	public function run();
+
+}
+
+// EOF
---
+++ app/Router.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * This file is part of Yolk - Gamer Network's PHP Framework.
+ *
+ * Copyright (c) 2015 Gamer Network Ltd.
+ * 
+ * Distributed under the MIT License, a copy of which is available in the
+ * LICENSE file that was bundled with this package, or online at:
+ * https://github.com/gamernetwork/yolk-contracts
+ */
+
+namespace yolk\contracts\app;
+
+/**
+ * A simple router interface.
+ */
+interface Router {
+
+	/**
+	 * Add a route.
+	 * @param bool  $regex    regular expresion that is matched against a request URI.
+	 * @param mixed $action   callback or closure associated with the route.
+	 * @param mixed $extra    extra data to be supplied to action not encoded in query.
+	 */
+	public function addRoute( $regex, $action, $extra = [] );
+
+	/**
+	 * Turn a controller action into a URL
+	 * @param string $action      controller action spec (like JobsController/index)
+	 * @param array  $args        positional arguments for url (e.g. job id) as strings
+	 */
+	public function reverse( $action, $args = [] );
+
+	/**
+	 * Find a route that matches the specified Request.
+	 * @param string $uri      URI to be matched.
+	 * @param string $method   HTTP method the URI request was made with.
+	 */
+	public function match( $uri, $method = 'GET' );
+
+}
+
+// EOF
